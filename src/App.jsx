0051b0f1978a74201f47623c0a680775e8c761f6
geradev66 <geradev66@gmail.com>
2025-01-15 20:17:55 -0600

    Desafio Hito 2 agregando nuevos archivos

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 import './App.css'
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Register from './components/Register';
+import Navbar from './components/Navbar';
+import Login from './components/Login'
 
 function App() {
   return (
     <>
-    <Home/>
+    {/* <Home/> */}
+    <Navbar/>
+    {/* <Register/> */}
+    <Login/>
     <Footer/>
     </>
   )
